b2dbf748192c5ce8f06d0496b7e21579d4910f90
kilych <kilych@zoho.com>
2017-05-04 03:34:03 +0700

    use express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require("http");
 
 const server = http.createServer(function(request, response) {
@@ -11,3 +12,26 @@ const server = http.createServer(function(request, response) {
 });
 
 server.listen(5000);
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    // response.write("#100днейкода (by kilych)");
+    response.send("100днейкода by kilych");
+    // response.end();
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
