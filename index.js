70cf9a5cfbea1d941e561681ed3bcaba367b859f
kilych <kilych@zoho.com>
2017-05-04 02:48:49 +0700

    change port

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http");
 
 const server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    response.write("#100днейкода (by kilych)");
-    response.end();
+    // if (request.headers['x-forwarded-proto'] == 'https')
+        // response.redirect(['http://', request.get('Host'), req.url].join(''));
+    // else {
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write("#100днейкода (by kilych)");
+        response.end();
+    // }
 });
 
-server.listen(5000);
+server.listen(3000);
