fa3b36e39532b16aadaee57a8bdd8ec519ac4b9a
kilych <kilych@zoho.com>
2017-05-04 03:15:03 +0700

    experiment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer(function(request, response) {
-    if (request.headers['x-forwarded-proto'] == 'https')
+    if (request.headers['X-Forwarded-Proto'] == 'https')
         response.redirect(['http://', request.get('Host'), request.url].join(''));
     else {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
