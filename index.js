68f3a0d8f733ae9296035e8c64f761614ab2451e
kilych <kilych@zoho.com>
2017-05-04 01:34:56 +0700

    Add author's name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    response.write("#100днейкода");
+    response.write("#100днейкода (by kilych)");
     response.end();
 });
 
