20ca85eff12d52404a3f883a77d555e7d35ef405
kilych <kilych@zoho.com>
2017-05-04 02:51:40 +0700

    add redirect

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require("http");
 
 const server = http.createServer(function(request, response) {
-    // if (request.headers['x-forwarded-proto'] == 'https')
-        // response.redirect(['http://', request.get('Host'), req.url].join(''));
-    // else {
+    if (request.headers['x-forwarded-proto'] == 'https')
+        response.redirect(['http://', request.get('Host'), request.url].join(''));
+    else {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
         response.write("#100днейкода (by kilych)");
         response.end();
-    // }
+    }
 });
 
-server.listen(3000);
+server.listen(5000);
