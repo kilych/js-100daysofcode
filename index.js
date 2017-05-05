780bebde6c14b0611ced64b9032e52cec3013ba8
kilych <kilych@zoho.com>
2017-05-06 02:54:20 +0700

    days 17, 18

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-/*
-const http = require("http");
-
-const server = http.createServer(function(request, response) {
-    if (request.headers['X-Forwarded-Proto'] == 'https')
-        response.redirect(['http://', request.get('Host'), request.url].join(''));
-    else {
-        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-        response.write("#100днейкода (by kilych)");
-        response.end();
-    }
-});
-
-server.listen(5000);
-*/
-
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    // response.write("#100днейкода (by kilych)");
-    response.send("#100днейкода (by kilych)");
-    // response.end();
+app.get('/17', function(request, response) {
+    response.send("#100днейкода (17th day by kilych)");
 });
+
+app.get('/18', function(req, res) {
+    res.sendFile(__dirname + '/days/18/18.html');
+});
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
