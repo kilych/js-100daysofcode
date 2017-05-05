2f7f636d8ff19bf8a523f548459abf33cc7efaf1
kilych <kilych@zoho.com>
2017-05-06 03:09:04 +0700

    socket.io chat fix 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var io = require('socket.io')(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
