2e3b4466d298c097b60ef43e534ff1ed0cb2b8e0
kilych <kilych@zoho.com>
2017-05-06 03:29:45 +0700

    socket.io chat fix 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/chat', function(req, res) {
 io.on('connection', function(socket) {
     // console.log('a user connected');
     socket.on('chat message', function(msg) {
-        // console.log('message: ' + msg);
+        console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
     // socket.on('disconnect', function() {
