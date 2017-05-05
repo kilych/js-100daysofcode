09a220099190cda1d609d49a9468ffde98d85758
kilych <kilych@zoho.com>
2017-05-06 03:05:40 +0700

    socket.io chat

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var app = express();
+var io = require('socket.io')(app);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/17', function(request, response) {
-    response.send("#100днейкода (17th day by kilych)");
+app.get('/', function(req, res) {
+    res.send("#100днейкода (17th day by kilych)");
+});
+
+app.get('/17', function(req, res) {
+    res.send("#100днейкода (17th day by kilych)");
 });
 
 app.get('/18', function(req, res) {
     res.sendFile(__dirname + '/days/18/18.html');
 });
 
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname + '/other/socketio-chat.html');
+});
+
+io.on('connection', function(socket) {
+    // console.log('a user connected');
+    socket.on('chat message', function(msg) {
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    // socket.on('disconnect', function() {
+    // console.log('user disconnected');
+    // });
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
