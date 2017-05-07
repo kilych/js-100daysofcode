094a796a037fbf0db0ca7c56f180fffdc161185b
kilych <kilych@zoho.com>
2017-05-08 02:32:44 +0700

    Add socket.io namespaces. Turn /19 into echo server

--- index.js
+++ index.js
@@ -17,29 +17,52 @@ app.get('/18', function(req, res) {
     res.sendFile(__dirname + '/days/18/18.html');
 });
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
+io.of('/chat').on('connection', function(socket) {
+    console.log('chat: a user connected');
+    // console.log('chat: a user ' + socket.id + ' connected');
     socket.on('chat message', function(msg) {
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
-        // socket.broadcast.emit(msg);
+        io.of('/chat').emit('chat message', msg);
     });
-    socket.on('moveon', function(msg) {
-        console.log('moveon: ' + msg);
-        io.emit('moveon', msg);
-        // socket.broadcast.emit(msg);
+    socket.on('disconnect', function() {
+        console.log('chat: user disconnected');
     });
-    // socket.on('disconnect', function() {
-    // console.log('user disconnected');
-    // });
 });
 
 app.get('/chat', function(req, res) {
     res.sendFile(__dirname + '/other/socketio-chat.html');
 });
 
+io.of('/19-echo').on('connection', function(socket) {
+    console.log('day19-echo_server: a user connected');
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        socket.emit('chat message', msg);
+    });
+    socket.on('disconnect', function() {
+        console.log('day19-echo_server: user disconnected');
+    });
+});
+
 app.get('/19', function(req, res) {
-    res.sendFile(__dirname + '/other/socketio-chat.html');
+    res.sendFile(__dirname + '/days/19/echo.html');
+});
+
+io.of('/20').on('connection', function(socket) {
+    console.log('day20: a user connected');
+    // socket.on('chat message', function(msg) {
+    //     console.log('message: ' + msg);
+    //     io.emit('chat message', msg);
+    //     // socket.broadcast.emit(msg);
+    // });
+    socket.on('moveon', function(msg) {
+        console.log('moveon: ' + msg);
+        io.emit('moveon', msg);
+        // socket.broadcast.emit(msg);
+    });
+    socket.on('disconnect', function() {
+        console.log('day20: user disconnected');
+    });
 });
 
 app.get('/20/input', function(req, res) {
