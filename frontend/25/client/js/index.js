d381394bdeb1b21c05cea44c3f5109afe69a2059
kilych <kilych@zoho.com>
2017-05-13 01:26:32 +0700

    Fix day 25

--- frontend/25/client/js/index.js
+++ frontend/25/client/js/index.js
@@ -1,13 +1,20 @@
+//
+// TODO: make tail of circle when it move via multiple tween
+// decreased alpha circles.
+// See: http://createjs.com/demos/tweenjs/tween_circles
+//
+
 "use strict";
 
 // init
 const width = 480, height = 320;
 const alpha = 0.3,
       clr = Math.random() * 360,
-      radius = 10 + 20 * Math.random();
-var oldX, oldY,
-    dragX, dragY,
-    objects = {};
+      radius = 10 + 20 * Math.random(),
+      // in milliseconds:
+      getInvisibilityTime = 100,
+      moveTime = 300;
+var objects = {};
 var socket = io.connect('/25');
 
 var canvas = document.getElementById('canvas');
@@ -16,13 +23,19 @@ canvas.height = height;
 //Create a stage by getting a reference to the canvas
 var stage = new createjs.Stage(canvas);
 stage.enableDOMEvents(true);
+// stage.enableMouseOver(10);
 
-var label = new createjs.Text("Your circle is in top left corner. Drag it.",
-                              "12px Sans-serif");
-stage.addChild(label);
+var text = new createjs.Text("Your circle is in center.\n" +
+                             "Click inside border to move it.",
+                             // "18px Arial",
+                             "18px Sans-serif",
+                             "#777");
+text.x = width/10;
+text.y = height/10;
+stage.addChild(text);
 
 socket.on('init', function(items) {
-    for(let key in items) {
+    for (let key in items) {
         makeCircle(items[key]);
     }
 });
@@ -32,49 +45,46 @@ var color = createjs.Graphics.getHSL(clr, 100, 50);
 circle.graphics.beginFill(color).drawCircle(0, 0, radius);
 circle.alpha = alpha;
 circle.radius = radius;
-//Set position of Shape instance:
-circle.x = 0;
-circle.y = 0;
+// Set position of Shape instance:
+circle.x = width/2;
+circle.y = height/2;
 
 socket.emit('newitem', {radius: radius, color: clr, x: circle.x, y: circle.y});
 
-circle.handleTick = function() {
-    this.x = dragX;
-    this.y = dragY;
-    // this.dx = (this.x - oldX);
-    // this.dy = (this.y - oldY);
-    // oldX = this.x;
-    // oldY = this.y;
-};
-
-circle.on("pressmove", dragObject);
-function dragObject(event) {
-    dragX = event.stageX;
-    dragY = event.stageY;
-    socket.emit('moveto', {x: dragX, y: dragY});
-}
+// circle.handleTick = function() {
+//     this.x = dragX;
+//     this.y = dragY;
+// };
 
 stage.addChild(circle);
 
-socket.on('newitem', function(item) {
-    // console.log(item);
-    makeCircle(item);
+socket.on('newitem', function(item) { makeCircle(item); });
+
+addEventListener("mouseup", function(event) {
+    if (0 < event.x && event.x < width && 0 < event.y && event.y < height) {
+        createjs.Tween.get(circle, {loop: false})
+            .to({ alpha: 0 }, getInvisibilityTime)
+            .to({ alpha: alpha, x: event.x, y: event.y},
+                moveTime,
+                // ((circle.y - event.y) * (circle.x - event.x)) * 0.001,
+                createjs.Ease.Linear);
+        socket.emit('moveto', {x: event.x, y: event.y});
+    }
 });
 
+// Update stage will render next frame
+createjs.Ticker.setFPS(50);
+createjs.Ticker.addEventListener("tick", stage);
+
 socket.on('moveto', function(point) {
-    // console.log(point);
-    let item = objects[point.id],
-        oldX = item.x,
-        oldY = item.y;
-    // item.alpha = 0;
+    let item = objects[point.id];
     createjs.Tween.get(item, { loop: false })
-        .to({ alpha: 0 }, 100)
-        .to({ alpha: alpha, x: point.x, y: point.y }, 300, createjs.Ease.linear);
-    item.x = point.x;
-    item.y = point.y;
-    // item.alpha = alpha;
-    // objects[point.id].x = point.x;
-    // objects[point.id].y = point.y;
+        .to({ alpha: 0 }, getInvisibilityTime)
+        .to({ alpha: alpha, x: point.x, y: point.y }, moveTime, createjs.Ease.linear)
+        .call(function() {
+            item.x = point.x;
+            item.y = point.y;
+        });
 });
 
 socket.on('delete', function(id) {
@@ -83,29 +93,15 @@ socket.on('delete', function(id) {
     delete objects[id];
 });
 
-//Update stage will render next frame
-createjs.Ticker.setFPS(50);
-createjs.Ticker.addEventListener("tick", handleTick);
-function handleTick() {
-    // for(var key in objects) { objects[key].handleTick(); }
-    circle.handleTick();
-    stage.update();
-}
-
 function makeCircle(item) {
-    // oldX = event.stageX;
-    // oldY = event.stageY;
-    // dragX = event.stageX;
-    // dragY = event.stageY;
     let circle = new createjs.Shape();
     let color = createjs.Graphics.getHSL(item.color, 100, 50);
     circle.graphics.beginFill(color).drawCircle(0, 0, item.radius);
     circle.alpha = 0.3;
     circle.radius = item.radius;
-    //Set position of Shape instance:
+    // Set position of Shape instance:
     circle.x = item.x;
     circle.y = item.y;
-    // circle.nowDragging = false;
 
     stage.addChild(circle);
     objects[item.id] = circle;
